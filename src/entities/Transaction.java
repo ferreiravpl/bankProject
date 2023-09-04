9dc9755ba9ddb243525ae2a379765fde9b6f9ca0
Lucas Ferreira <ferreiravpl@gmail.com>
2023-09-04 20:14:15 -0300

    adjusting Transaction entity

--- src/entities/Transaction.java
+++ src/entities/Transaction.java
@@ -1,5 +1,25 @@
 package entities;
 
+import static entities.Account.balance;
+
 public class Transaction {
 
+    private double deposit;
+    private double withdraw;
+
+    public double getDeposit() {
+        return deposit + balance;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public double getWithdraw() {
+        return withdraw;
+    }
+
+    public void setWithdraw(double withdraw) {
+        Account.setBalance(Account.getBalance() - withdraw - 5);
+    }
 }
