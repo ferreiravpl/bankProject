dafb32d780b0574d833563ed642adffbc59821b0
Lucas Ferreira <ferreiravpl@gmail.com>
2023-09-09 11:07:54 -0300

    Programa final

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
 import entities.Account;
+import entities.Transaction;
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Transaction;
 
 public class Program {
 
@@ -22,35 +22,34 @@ public class Program {
         ac.setAccountNumber(accountNumber);
         sc.nextLine();
 
+        Transaction tc = new Transaction();
+
         //Questionando se há depósito inicial
         System.out.print("Is there a initial deposit (y/n)? ");
         String isInitialDeposit = sc.nextLine();
 
-        Transaction tc = new Transaction();
-
         //Validação do depósito inicial e inserção do valor no balanço final da conta
-        if ("y".equalsIgnoreCase(isInitialDeposit)) {
-            System.out.print("Enter the initial deposit value: ");
-            double initialDeposit = sc.nextDouble();
-            tc.setInitialDeposit(initialDeposit);
-            tc.setBalance(initialDeposit);
-            System.out.println("Account data with deposit: " + ac);
+        if ("n".equalsIgnoreCase(isInitialDeposit)) {
+            System.out.println("Account data without deposit: " + "Account owner: " + ac.getName() + ", " + "Account number: " + ac.getAccountNumber() + ", " + "Account balance: " + ac.getBalance());
         } else {
-            System.out.println("Account data without deposit: " + ac);
+            System.out.print("Enter the initial deposit value: ");
+            double initialDepositValue = sc.nextDouble();
+            ac.setInitialDeposit(initialDepositValue);
+            ac.setBalance(initialDepositValue);
+            System.out.println("Updated account data: " + "Account owner: " + ac.getName() + ", " + "Account number: " + ac.getAccountNumber() + ", " + "Account balance: " + ac.getBalance());
         }
 
         //Questionando depósito posterior
         System.out.print("Enter a deposit value: ");
         double depositValue = sc.nextDouble();
         tc.setDeposit(depositValue);
-        System.out.println(tc.getDeposit());
-        System.out.println("Updated account data: " + ac);
+        System.out.println("Updated account data: " + "Account owner: " + ac.getName() + ", " + "Account number: " + ac.getAccountNumber() + ", " + "Account balance: " + ac.getBalance());
 
         //Questionando saque
         System.out.print("Enter a withdraw value: ");
-        double withdrawValue = sc.nextDouble();
-        tc.setWithdraw(withdrawValue);
-        System.out.println("Updated account data: " + ac);
+        double withdraw = sc.nextDouble();
+        tc.setWithdraw(withdraw);
+        System.out.println("Updated account data: " + "Account owner: " + ac.getName() + ", " + "Account number: " + ac.getAccountNumber() + ", " + "Account balance: " + ac.getBalance());
 
         sc.close();
 
